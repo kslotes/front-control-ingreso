08851ee77a859d645322c1a073edbb6e1ffe6197
Gustavo <gviglesias@alumnos.unsada.edu.ar>
2021-05-11 12:49:18 -0300

    factorriesgo

--- src/components/Ddjj.jsx
+++ src/components/Ddjj.jsx
@@ -3,11 +3,19 @@ import {Button, Form} from "react-bootstrap";
 import axios from "axios";
 import "./Formulario.css";
 
+
 const Ddjj = () => {
     
     const [preguntas, setPreguntas] = useState([]);
     const [value, setValue] = useState([]);
     const [factorDeRiesgo, setFactorDeRiesgo] = useState([]);
+
+
+    const handleChangeFactor=(event)=>{
+        const name = event.target.name
+        const valor = event.target.value
+        setValue((value) => [...value, {[name]: valor}]);
+    }
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(value)
@@ -22,11 +30,16 @@ const Ddjj = () => {
         axios.get(`http://areco.gob.ar:9528/api/pregunta/all`).then((res) => {
             setPreguntas(res.data.data);
         });
+        axios.get(`http://areco.gob.ar:9528/api/factorderiesgo/all`).then((res) => {
+            setFactorDeRiesgo(res.data.data);
+        });
     }, []);
     
     // console.log(`Respuestas: {\n\t nameCheck: ${value.name}\n\t idPregunta:${value.idPregunta},\n\t afirmativo: ${value.afirmativo}\n\t}`);
     console.log(value)
+    console.log(factorDeRiesgo)
     return (
+        <>
         <Form className="seccion-container mb-3" onSubmit={handleSubmit}>
             <h2>Declaración Jurada</h2>
             {preguntas.map((pregunta, i) => {
@@ -61,6 +74,27 @@ const Ddjj = () => {
                     Manejar cuando intercambian de opcion en cada pregunta
                     Antes de hacer el PUT aplicar .filter() al array que usamos*/}
         </Form>
+        
+        <Form className="seccion-container mb-3" onSubmit={handleSubmit}>
+        <h2>Si tu situacion de salud contempla alguna de las siguientes opciones,seleccione algunas de las siguientes opciones </h2>
+            {factorDeRiesgo.map((data,i)=>{
+                return(
+                    <>
+                    <Form.Label className="label-preguntas mt-3">{factorDeRiesgo.nombre}</Form.Label>
+                        <Form.Check
+                            xs={6}
+                            name={"factor"+i}
+                            label="Si"
+                            key={i}
+                            type="radio"
+                          
+                            onChange={handleChangeFactor}
+                        />
+                    </>
+                )
+            })}
+        </Form>
+       </>
     );
 };
 
