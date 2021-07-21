f0d41aaecf7e0b6a2f38f18877bb9c14d9505f7a
kslotes <44619076+kslotes@users.noreply.github.com>
2021-07-21 18:20:58 -0300

    Cambia valorDefault y persiste a BDD

--- src/components/Cohortes/ModificarCohorte.jsx
+++ src/components/Cohortes/ModificarCohorte.jsx
@@ -4,25 +4,38 @@ import * as Api from "../Api.js";
 
 const ModificarCohorte = (props) => {
     const [showModal, setShowModal] = useState(false);
-    const [cohorte] = useState(props.cohorte);
+    const [cohorte, setCohorte] = useState(props.cohorte);
     const [sedes, setSedes] = useState([]);
-    const [nuevoNombre, setNuevoNombre] = useState()
-    const [nuevaFechaInicio, setNuevaFechaInicio] = useState()
-    const [nuevaFechaFin, setNuevaFechaFin] = useState()
+    const [nuevoNombre, setNuevoNombre] = useState(props.cohorte.nombre);
+    const [nuevaFechaInicio, setNuevaFechaInicio] = useState(props.cohorte.fechaInicio);
+    const [nuevaFechaFin, setNuevaFechaFin] = useState(props.cohorte.fechaFin);
 
     const handleNuevoNombre = (event) => setNuevoNombre(event.target.value);
     const handleNuevaFechaInicio = (event) => setNuevaFechaInicio(event.target.value);
     const handleNuevaFechaFin = (event) => setNuevaFechaFin(event.target.value);
 
-    const handleSubmit = () => {};
+    const handleSubmit = () => {
+        console.log(props.cohorte.idCohorte, nuevoNombre, nuevaFechaInicio, nuevaFechaFin);
+        Api.updateCohorte(props.cohorte.idCohorte, nuevoNombre, nuevaFechaInicio, nuevaFechaFin);
+        setShowModal(false);
+    };
+
+    // * useEFfects
+
     useEffect(() => {
         setShowModal(props.showModal);
     }, [props.showModal]);
+
+    useEffect(() => {
+        setCohorte(props.cohorte);
+    }, [props.cohorte]);
+
     useEffect(() => {
         Api.getSedes().then((res) => {
-            setSedes(res)
-        })
-    }, [])
+            setSedes(res);
+        });
+    }, []);
+
     return (
         <div>
             <Modal show={showModal} onHide={props.handleClose}>
@@ -30,6 +43,14 @@ const ModificarCohorte = (props) => {
                     <Modal.Title>Modificar Cohorte</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    <Form.Group controlId="formSede" className="mt-2">
+                        <Form.Label>Sede</Form.Label>
+                        <Form.Control type="text" value={cohorte.sede.nombre} readOnly />
+                    </Form.Group>
+                    <Form.Group controlId="formActividad" className="mt-2">
+                        <Form.Label>Actividad</Form.Label>
+                        <Form.Control type="text" value={"Matematica III"} readOnly />
+                    </Form.Group>
                     <Form.Group controlId="formNombreCohorte" className="mt-2">
                         <Form.Label>Nombre</Form.Label>
                         <Form.Control type="text" defaultValue={cohorte.nombreCohorte} onChange={handleNuevoNombre} autoComplete="off" />
