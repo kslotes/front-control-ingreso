bf7f272d97127ca31d053d1fe9e5f24b675c1db6
kslotes <44619076+kslotes@users.noreply.github.com>
2021-07-21 13:10:23 -0300

    Comienza funcionalidad de modificar cohortes

--- src/components/Cohortes/ModificarCohorte.jsx
+++ src/components/Cohortes/ModificarCohorte.jsx
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from "react";
 import {Modal, Form, Button} from "react-bootstrap";
+import * as Api from "../Api.js";
 
 const ModificarCohorte = (props) => {
     const [showModal, setShowModal] = useState(false);
     const [cohorte] = useState(props.cohorte);
+    const [sedes, setSedes] = useState([]);
     const [nuevoNombre, setNuevoNombre] = useState()
     const [nuevaFechaInicio, setNuevaFechaInicio] = useState()
     const [nuevaFechaFin, setNuevaFechaFin] = useState()
@@ -16,6 +18,11 @@ const ModificarCohorte = (props) => {
     useEffect(() => {
         setShowModal(props.showModal);
     }, [props.showModal]);
+    useEffect(() => {
+        Api.getSedes().then((res) => {
+            setSedes(res)
+        })
+    }, [])
     return (
         <div>
             <Modal show={showModal} onHide={props.handleClose}>
@@ -25,15 +32,15 @@ const ModificarCohorte = (props) => {
                 <Modal.Body>
                     <Form.Group controlId="formNombreCohorte" className="mt-2">
                         <Form.Label>Nombre</Form.Label>
-                        <Form.Control type="text" value={cohorte.nombreCohorte} placeholder="Ingrese nuevo nombre" onChange={handleNuevoNombre} autoComplete="off" />
+                        <Form.Control type="text" defaultValue={cohorte.nombreCohorte} onChange={handleNuevoNombre} autoComplete="off" />
                     </Form.Group>
                     <Form.Group controlId="form" className="mt-2">
                         <Form.Label>Fecha Inicio</Form.Label>
-                        <Form.Control type="date" value={cohorte.fechaInicioCohorte} onChange={handleNuevaFechaInicio} />
+                        <Form.Control type="date" defaultValue={cohorte.fechaInicio} onChange={handleNuevaFechaInicio} />
                     </Form.Group>
                     <Form.Group controlId="form" className="mt-2">
                         <Form.Label>Fecha Fin</Form.Label>
-                        <Form.Control type="date" value={cohorte.fechaFinCohorte} onChange={handleNuevaFechaFin} />
+                        <Form.Control type="date" defaultValue={cohorte.fechaFin} onChange={handleNuevaFechaFin} />
                     </Form.Group>
                 </Modal.Body>
                 <Modal.Footer>
