41ee21ef66576c4a7ca2efcec52e417f64c120b2
kslotes <44619076+kslotes@users.noreply.github.com>
2021-07-21 13:10:57 -0300

    Refactoriza componente

--- src/components/Cohortes/NuevoCohorte.jsx
+++ src/components/Cohortes/NuevoCohorte.jsx
@@ -1,64 +1,113 @@
 import {Form, Button, Modal, Table} from 'react-bootstrap'
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-import {URL_BASE, API_GET_SEDES, API_GET_DEPENDENCIAS} from '../Api.js'
+import * as Api from '../Api.js'
 import Swal from 'sweetalert2'
+
+/*
+ TODO: Resolver asignacion de horarios
+    * Ya esta armado el array de horarios, falta arreglar que la tabla renderice correctamente
+ TODO: Al crear un cohorte, asignar al horario el idCohorte devuelto por la api
+ TODO: Agregar a la tabla de cohortes los horarios asignados
+*/
+
+const DIAS_SEMANA = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+const MODALIDADES = ["Practico", "Teorico", "Teorico-Practico", "Actividad Extracurricular"]
+
+let arrHorarios = [];
+let placeholder = "Ingrese una";
 
 export default (props) => {
 
-    const DIAS_SEMANA = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
-    const MODALIDADES = ["Practico", "Teorico", "Teorico-Practico", "Actividad Extracurricular"]
-
-    let horarios = [];
-    const [showTable, setShowTable] = useState(false);
-    const [horariosCohorte, setHorariosCohorte] = useState([]);
+    // * Parametros
     const [show, setShow] = useState(false);
-    const placeholder = "Seleccione una"
-    const [nombre, setNombre] = useState();
     const [sedes, setSedes] = useState([]);
-    const [selectedSede, setSelectedSede] = useState();
     const [dependencias, setDependencias] = useState([]);
     const [propuestas, setPropuestas] = useState([]);
     const [actividades, setActividades] = useState([]);
-    const [selectedActividad, setSelectedActividad] = useState();
-    const [selectedFechaInicio, setSelectedFechaInicio] = useState();
-    const [selectedFechaFin, setSelectedFechaFin] = useState();
+    const [cohortes, setCohortes] = useState([]);
 
-    const [diaCohorte, setDiaCohorte] = useState();
-    const [horaInicioCohorte, setHoraInicioCohorte] = useState();
-    const [horaFinCohorte, setHoraFinCohorte] = useState();
+    // * Cohorte
+    const [nombre, setNombre] = useState('');
+    const [sede, setSede] = useState();
+    const [dependencia, setDependencia] = useState();
+    const [propuesta, setPropuesta] = useState();
+    const [actividad, setActividad] = useState();
+    const [fechaInicio, setFechaInicio] = useState();
+    const [fechaFin, setFechaFin] = useState();
+
+    // * Horario
+    const [dia, setDia] = useState();
+    const [horaInicio, setHoraInicio] = useState();
+    const [horaFin, setHoraFin] = useState();
     const [modalidad, setModalidad] = useState();
+    const [showTable, setShowTable] = useState();
+    const [horarios, setHorarios] = useState(arrHorarios);
     
-    const handleShow = () => setShow(true);
-    const handleClose = () => setShow(false);
+    // * Extra
+    const [cohorteCreado, setCohorteCreado] = useState();
+
+    // * Eventos
     const handleNombre = (event) => setNombre(event.target.value);
-    
+    const handleSede = (event) => {
+        setSede(event.target.value)
+    };
+    const handleDependencia = (event) => {
+        setDependencia(event.target.value)
+        Api.getPropuestasByDependencia(event.target.value).then(data => {
+            setPropuestas(data)
+        })
+    };
+    const handlePropuesta = event => {
+        setPropuesta(event.target.value)
+        Api.getActividadesByPropuesta(event.target.value).then(data => {
+            setActividades(data)
+        })
+    };
+    const handleActividad = event => setActividad(event.target.value);
+    const handleFechaInicio = event => setFechaInicio(event.target.value);
+    const handleFechaFin = event => setFechaFin(event.target.value);
+    const handleDia = event => setDia(event.target.value);
+    const handleHoraInicio = event => setHoraInicio(event.target.value);
+    const handleHoraFin = event => setHoraFin(event.target.value);
     const handleModalidad = event => setModalidad(event.target.value);
-    const handleHoraInicioCohorte = event => setHoraInicioCohorte(event.target.value);
-    const handleHoraFinCohorte = event => setHoraFinCohorte(event.target.value);
-    const handleDiaCohorte = event => setDiaCohorte(event.target.value);
-
-    const handleAsignar = async () => {
-    }
-    const handleSelectSede = (event) => setSelectedSede(event.target.value);
-    const handleSelectDependencia = async (event) => {
-    
-    }
-
-    const handleSelectPropuesta = async (event) => {
-       
-    }
-    const handleSelectActividad = (event) => setSelectedActividad(event.target.value);
-    const handleSelectedFechaInicio = (event) => {
-        setSelectedFechaInicio(event.target.value); 
-    }
-    const handleSelectedFechaFin = (event) => setSelectedFechaFin(event.target.value); 
+    const handleCohorte = event => setCohorteCreado(event.target.value);
     const handleSubmit = async () => {
     }
 
+    const handleCrearCohorte = async () => {
+        console.log(`${nombre}, ${sede}, ${dependencia}, ${propuesta}, ${actividad}, ${fechaInicio}, ${fechaFin}`)
+        console.log(Api.addCohorte(actividad, sede, nombre, fechaInicio, fechaFin).data)
+    }
+    const handleAsignarHorario = async () => {
+        setShowTable(true);
+        arrHorarios.push(
+            {
+                dia: dia,
+                horaInicio: horaInicio,
+                horaFin: horaFin,
+                modalidad: modalidad
+            }
+        )
+        console.log('arrHorarios:', arrHorarios);
+        setHorarios(arrHorarios);
+        console.log('horarios state', horarios);
+    }
     useEffect(() => {
         setShow(props.showModal);
     }, [props.showModal]);
+
+    useEffect(() => {
+        Api.getSedes().then((res) => {
+            setSedes(res)
+        })
+        Api.getDependencias().then((res) => {
+            setDependencias(res)
+        })
+        Api.getCohortes().then((res) => {
+            setCohortes(res)
+        })
+    }, [])
     return (
         <>
           <Modal show={show} onHide={props.handleHide}>
@@ -66,75 +115,88 @@ export default (props) => {
               <Modal.Title>Crear Cohorte</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                    <Form.Group controlId="formNombreCohorte" className="mt-2">
+                    <Form.Group controlId="formNombre" className="mt-2">
                         <Form.Label>Nombre</Form.Label>
-                        <Form.Control onChange={handleNombre} autoComplete="off" type="text" placeholder="Ingrese nombre" required/>
+                        <Form.Control onChange={handleNombre} autoComplete="off" type="text" placeholder="Ejemplo: Primer Cuatrimestre" required/>
                     </Form.Group>
-                    <Form.Group controlId="formSedeCohorte"className="mt-2">
+                    <Form.Group controlId="formSede"className="mt-2">
                         <Form.Label>Sede</Form.Label>
-                        <Form.Control onChange={handleSelectSede} defaultValue={placeholder} as="select">
+                        <Form.Control onChange={handleSede} defaultValue={placeholder} as="select" required>
                             <option disabled value={placeholder}>{placeholder}</option>
                             {sedes.map(sede => <option key={sede.idSede} value={sede.idSede}>{sede.nombre}</option>)}
                         </Form.Control>
                     </Form.Group>
-                    <Form.Group controlId="formDependenciaCohorte"className="mt-2">
+                    <Form.Group controlId="formDependencia"className="mt-2">
                         <Form.Label>Dependencia: </Form.Label>
-                        <Form.Control as="select" defaultValue={placeholder} onChange={handleSelectDependencia}>
+                        <Form.Control as="select" defaultValue={placeholder} onChange={handleDependencia} required>
                             <option disabled value={placeholder}>{placeholder}</option>
                             {dependencias.map(dependencia => <option key={dependencia.idDependencia} value={dependencia.idDependencia}>{dependencia.nombre}</option>)}
                         </Form.Control>
                     </Form.Group>
-                    <Form.Group controlId="formPropuestaCohorte"className="mt-2">
+                    <Form.Group controlId="formPropuesta"className="mt-2">
                         <Form.Label>Propuesta </Form.Label>
-                        <Form.Control as="select" defaultValue={placeholder} onChange={handleSelectPropuesta}>
+                        <Form.Control as="select" defaultValue={placeholder} onChange={handlePropuesta} required>
                             <option disabled value={placeholder}>{placeholder}</option>
                             {propuestas.map(propuesta => <option key={propuesta.idPropuesta} value={propuesta.idPropuesta}>{propuesta.nombre}</option>)}
                         </Form.Control>
                     </Form.Group>
-                    <Form.Group controlId="formActividadCohorte"className="mt-2">
+                    <Form.Group controlId="formActividad"className="mt-2">
                         <Form.Label>Actividad </Form.Label>
-                        <Form.Control as="select" defaultValue={placeholder} onChange={handleSelectActividad}>
+                        <Form.Control as="select" defaultValue={placeholder} onChange={handleActividad} required>
                             <option disabled value={placeholder}>{placeholder}</option>
                             {actividades.map(actividad => <option key={actividad.idActividad} value={actividad.idActividad}>{actividad.nombre}</option>)}
                         </Form.Control>
                     </Form.Group>
-                    <Form.Group controlId="formFechaInicioCohorte"className="mt-2">
+                    <Form.Group controlId="formFechaInicio"className="mt-2">
                         <Form.Label>Fecha Inicio</Form.Label>
-                        <Form.Control type="date"  onChange={handleSelectedFechaInicio}/>
+                        <Form.Control type="date"  onChange={handleFechaInicio} required/>
                     </Form.Group>
 
-                    <Form.Group controlId="formFechaFinCohorte"className="mt-2">
+                    <Form.Group controlId="formFechaFin"className="mt-2">
                         <Form.Label>Fecha Fin</Form.Label>
-                        <Form.Control type="date"  onChange={handleSelectedFechaFin}/>
+                        <Form.Control type="date"  onChange={handleFechaFin} required/>
                     </Form.Group>
+                    <Button className="mt-3" variant="primary" onClick={handleCrearCohorte}>Crear Cohorte</Button>
                 </Modal.Body>
                     <Modal.Header>
                         <Modal.Title>Asignar Dias y Horarios</Modal.Title>
                     </Modal.Header>
                 <Modal.Body>
-                    <Form.Group controlId="formDiaCohorte" className="mt-2">
+                    <Form.Group controlId="formCohorte" className="mt-2">
+                        <Form.Label>Cohorte</Form.Label>
+                        <Form.Control as="select" onChange={handleCohorte} required>
+                            <option key='blank' hidden value>Seleccione uno</option>
+                            {cohortes.map(cohorte =>
+                                <option
+                                    key={cohorte.idCohorte}
+                                    value={cohorte.idCohorte}>
+                                        {`${cohorte.idCohorte} - ${cohorte.nombreCohorte}`}
+                                </option>)}
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="formDia" className="mt-2">
                         <Form.Label>Dia</Form.Label>
-                        <Form.Control as="select" defaultValue="Seleccione uno" onChange={handleDiaCohorte}>
-                            <option disabled>Seleccione uno</option>
+                        <Form.Control as="select" onChange={handleDia}>
+                            <option key="blank" hidden value>Seleccione uno</option>
                             {DIAS_SEMANA.map((d) => <option key={d} value={d}>{d}</option>)}
                         </Form.Control>
                     </Form.Group>
-                    <Form.Group controlId="formHoraInicioCohorte" className="mt-2">
+                    <Form.Group controlId="formHoraInicio" className="mt-2">
                         <Form.Label>Hora Inicio</Form.Label>
-                        <Form.Control type="time"value={horaInicioCohorte} onChange={handleHoraInicioCohorte}/>
+                        <Form.Control type="time"value={horaInicio} onChange={handleHoraInicio}/>
                     </Form.Group>
-                    <Form.Group controlId="formHoraFinCohorte" className="mt-2">
+                    <Form.Group controlId="formHoraFin" className="mt-2">
                         <Form.Label>Hora Fin</Form.Label>
-                        <Form.Control type="time"value={horaFinCohorte} onChange={handleHoraFinCohorte}/>
+                        <Form.Control type="time"value={horaFin} onChange={handleHoraFin}/>
                     </Form.Group>
-                    <Form.Group controlId="formModalidadCohorte" className="mt-2">
+                    <Form.Group controlId="formModalidad" className="mt-2">
                         <Form.Label>Modalidad</Form.Label>
                         <Form.Control as="select" onChange={handleModalidad} defaultValue="Seleccione una">
                             <option disabled>Seleccione una</option>
                             {MODALIDADES.map((m) => <option key={m} value={m}>{m}</option>)}
                         </Form.Control>
                     </Form.Group>
-                    <Button className="mt-3" variant="primary" onClick={handleAsignar}>Asignar</Button>
+                    <Button className="mt-3" variant="primary" onClick={handleAsignarHorario}>Asignar Horario</Button>
                     <Table striped bordered className="mt-2" hidden={!showTable}>
                         <thead>
                             <tr>
@@ -148,7 +210,10 @@ export default (props) => {
                             {horarios.map((horario) => {
                                 return (
                                     <tr>
-                                        <td>{horario.diaCohorte}</td>
+                                        <td>{horario.dia}</td>
+                                        <td>{horario.modalidad}</td>
+                                        <td>{horario.horaInicio}</td>
+                                        <td>{horario.horaFin}</td>
                                     </tr>
                                 )
                             })}
