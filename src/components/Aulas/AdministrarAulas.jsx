9b139fb3c1a386e6b6c53fcd5b3dc52a618993e4
kslotes <44619076+kslotes@users.noreply.github.com>
2021-07-22 17:30:10 -0300

    Refactoriza a nueva tabla

--- src/components/Aulas/AdministrarAulas.jsx
+++ src/components/Aulas/AdministrarAulas.jsx
@@ -1,27 +1,16 @@
 import {Container, Col} from 'react-bootstrap'
 import NavBarTop from '../NavBar/NavBarTop'
-import TablaAulas from './TablaAulas'
-import {useState, useEffect} from 'react'
-import {API_GET_AULAS} from '../Api.js'
-import axios from 'axios'
-import ModalNuevaAula from './ModalNuevaAula'
+import TablaAulas2 from './TablaAulas2'
+
 
 export default () => {
-    const [aulas, setAulas] = useState([]);
 
-    useEffect(() => {
-        axios.get(API_GET_AULAS)
-            .then(res => {
-                setAulas(res.data.data);
-            })
-    }, [])
     return (
         <Container fluid className="fondo">
             <NavBarTop/>
             <Col className="seccion-container">
                 <h2 className="texto-h2">Listado de Aulas</h2>
-                <ModalNuevaAula/>
-                <TablaAulas aulas={aulas}/>
+                <TablaAulas2/>
             </Col>
         </Container>
     )
