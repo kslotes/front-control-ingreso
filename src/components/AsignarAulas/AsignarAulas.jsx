4f1f457b1a9b6083c2fbabb2ee9b55a88dac5f15
kslotes <44619076+kslotes@users.noreply.github.com>
2021-07-23 15:51:43 -0300

    Arregla path de modal

--- src/components/AsignarAulas/AsignarAulas.jsx
+++ src/components/AsignarAulas/AsignarAulas.jsx
@@ -1,6 +1,6 @@
 import {Container, Col, Button} from "react-bootstrap";
 import NavBarTop from "../NavBar/NavBarTop";
-import ModalAsignarAula from "../Aulas/ModalAsignarAula";
+import ModalAsignarAula from "./ModalAsignarAula";
 import {useState} from "react";
 import TablaSesiones from "./TablaSesiones";
 export default () => {
