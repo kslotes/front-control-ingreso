f670cde865b37669abf070b8862db9b9a3188ac0
Agustin Moro <nitsugaorom@gmail.com>
2021-07-28 21:13:20 -0300

    Crea componente Login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import NavBarTop from '../NavBar/NavBarTop'
+const Login = () => {
+    return (
+        <Container fluid className="fondo">
+            <NavBarTop />
+        </Container>
+    )
+}
+
+export default Login
